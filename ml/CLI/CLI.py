a6f615bfc2f5b7fc29e4875860ac1ad4f53b4f48
Bunsans <sandr474@gmail.com>
2022-03-16 19:50:35 +0300

    updated class Quote

--- ml/CLI/CLI.py
+++ ml/CLI/CLI.py
@@ -11,22 +11,28 @@ class Quote:
     user_feelings = ""
     list_for_model = []
     processed_feelings = ""
+    path_to_csv = ""
+    path_to_model = ""
 
-    def __init__(self):
+    def __init__(self, path_to_csv, path_to_model):
+        self.path_to_csv = path_to_csv
+        self.path_to_model = path_to_model
         # files:
         # Create lemmatizer and stopwords list
         self.mystem = Mystem()
         self.russian_stopwords = stopwords.words("russian")
         # reading csv
-        df = pd.read_csv('processed_q.csv', engine='python')
+        # processed_q.csv
+        df = pd.read_csv(path_to_csv, engine='python')
         # lists of quotes
         self.quote_tokens = [q.split()
                              for q in df['processed_quotes'].values.tolist()]
         self.quote_words = [q.split() for q in df['quotes'].values.tolist()]
 
         # model downloading
+        # models/d2v_v1-0.model
+        self.model_d2v = gensim.models.doc2vec.Doc2Vec.load(path_to_model)
 
-        self.model_d2v = gensim.models.doc2vec.Doc2Vec.load('models/d2v_v1-0.model')
         # suggested quotes
         self.Q_NUMBER = 1
 
@@ -50,7 +56,6 @@ class Quote:
 def createParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('text', nargs=1)
-
     return parser
 
 parser = createParser()
@@ -58,6 +63,10 @@ namespace = parser.parse_args (sys.argv[1:])
 
 # Эту строчку можно закомментить после первого запуска
 nltk.download("stopwords")
-p = Quote()
+# при инициализации нужно указывать сначала путь к csv, потом к моделям, насколько я понял нужен полный путь
+# через относительный не смог найти как это сделать. Можно в теории использовать pathlib, но пока хз насколько имеет смысл это делать
+
+p = Quote("/home/alex/4sem/Project/PsychoBot/ml/CLI/processed_q.csv", "/home/alex/4sem/Project/PsychoBot/ml/CLI/models/d2v_v1-0.model")
 p.preprocess_text("".join(namespace.text))
+p.basic_model()
 p.basic_model()
